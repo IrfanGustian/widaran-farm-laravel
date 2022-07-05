f6f60b55548bbe6b42c26ae683f17ba702379b7c
IrfanGustian <irfangustian46@gmail.com>
2022-07-05 10:58:50 +0700

    menabahkan search dan pagination

--- app/Http/Controllers/SapiController.php
+++ app/Http/Controllers/SapiController.php
@@ -8,8 +8,20 @@ use App\Models\Sapi;
 class SapiController extends Controller
 {
     public function index(){
+        $sapi = Sapi::latest();
+        $sapi_slider = Sapi::all();
+
+        if (request('search')) {
+            $sapi->where('nama_sapi', 'like', '%' . request('search') . '%')
+                ->orWhere('deskripsi', 'like', '%' . request('search') . '%')
+                ->orWhere('harga', 'like', '%' . request('search') . '%');
+        }
+
+        request('search');
+
         return view('daftarsapi', [
-            "data_sapi" => Sapi::all()
+            "data_sapi" => $sapi->paginate(6),
+            "sapi_slider" => $sapi_slider
         ]);
     }
 
--- resources/views/daftarsapi.blade.php
+++ resources/views/daftarsapi.blade.php
@@ -40,48 +40,87 @@
     <!-- Women Banner Section Begin -->
     <section class="women-banner spad">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col">
-                    <h3 class="text-center font-weight-bold mt-5">
-                        DAFTAR SAPI
-                    </h3>
+                <h3 class="text-center mb-3 font-weight-bold mt-5">
+                    DAFTAR SAPI
+                </h3>
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <form action="/daftarsapi">
+                        <div class="input-group mb-3">
+                            <input type="text" name="search" class="form-control" value="{{ request('search') }}" placeholder="Search...">
+                            <button class="btn btn-outline-secondary" type="submit" id="">Search</button>
+                          </div>
+                    </form>
                 </div>
                 
             </div>
             <div class="row">
                 <div class="col-lg-12 mt-5">
-                    <div class="product-slider owl-carousel">
 
-                        @foreach ($data_sapi as $sapi)
-
-                            @empty($sapi->trans_id)
+                    @if ($data_sapi->count())
+                            @if (!request('search'))
+                                
+                            <div class="product-slider owl-carousel mb-4">
+                                
+                                @foreach ($sapi_slider as $sapi)
+                                
+                                @empty($sapi->trans_id)
+                                
                                 <div class="product-item">
-
-
                                     <div class="pi-pic">
-                                        <img src="{{ asset('storage/'. $sapi->img) }}" class="img-fluid d-block mb-3 col-sm-10" />
-                                    </div>
-                                    <div class="pi-text">
-                                        <a href="#">
-                                            <h5>{{ $sapi->nama_sapi }}</h5>
-                                        </a>
-                                        <div class="catagory-name">{{ $sapi->deskripsi }}</div>                              
-                                        <div class="product-price">Rp. 
-                                            {{ number_format($sapi->harga, 0, ',', '.')  }}
+                                                <img src="{{ asset('storage/'. $sapi->img) }}" class="img-fluid d-block mb-3 col-sm-10" />
+                                            </div>
+                                            <div class="pi-text">
+                                                <a href="#">
+                                                    <h5>{{ $sapi->nama_sapi }}</h5>
+                                                </a>
+                                                <div class="catagory-name">{{ $sapi->deskripsi }}</div>                              
+                                                <div class="product-price">Rp. 
+                                                    {{ number_format($sapi->harga, 0, ',', '.')  }}
+                                                </div>
+                                                
+                                            </div>
+                                            <div class="button-pesan">
+                                                <a href="/pembayaran/{{ $sapi->slug }}" class="primary-btn">Pesan</a>
+                                            </div>
                                         </div>
+                                        @endempty
                                         
+                                        @endforeach                  
                                     </div>
-                                    <div class="button-pesan">
-                                        <a href="/pembayaran/{{ $sapi->slug }}" class="primary-btn">Pesan</a>
-                                    </div>
-                                </div>
-                            @endempty
+                                    
+                        @endif
+                        <div class="container">
+                            <div class="row">
+                                @foreach ($data_sapi as $sapilagi)
 
-                        @endforeach
+                                    @empty($sapilagi->trans_id)
+                                        <div class="d-flex mb-5 px-3 mx-auto">
+                                            <div class="card border-light" style="width: 18rem;">
+                                                <img src="{{ asset('storage/'. $sapilagi->img) }}" class="card-img-to" alt="...">
+                                                <div class="card-body">
+                                                    <h5 class="card-title">{{ $sapilagi->nama_sapi }}</h5>
+                                                    <p class="card-text">{{ $sapilagi->deskripsi }}</p>
+                                                    <div class="product-price">Rp. 
+                                                        {{ number_format($sapilagi->harga, 0, ',', '.')  }}
+                                                    </div>
+                                                    <div class="button-pesan">
+                                                        <a href="/pembayaran/{{ $sapilagi->slug }}" class="primary-btn">Pesan</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endempty
 
-                        
+                                @endforeach
+                            </div>
+                        </div>
+                    @else
+                        <p class="text-center fs-4">Data Sapi Tidak Ada</p>
+                    @endif
 
-                                          
+                    <div class="d-flex justify-content-center">
+                     {{ $data_sapi->links() }}
                     </div>
                 </div>
             </div>
